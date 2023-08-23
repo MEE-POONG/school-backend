6c8cc075c0e0109f7de141fabeaa939b1abb0cb6
wechanon <wechanon2001@gmail.com>
2023-08-23 10:20:45 +0700

    tiw

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import React,{ useEffect } from "react";
 import Head from "next/head";
 import type { AppProps } from 'next/app'
 import { useRouter } from "next/router";
-import { SSRProvider } from "react-bootstrap";
 // import 'bootstrap/dist/js/bootstrap';
 // import './../scss/globals.scss'
 import '../scss/globals.scss'
@@ -16,7 +15,6 @@ export default function App({ Component, pageProps }: AppProps) {
   }, [router.events]);
 
   return (
-    <SSRProvider>
       <React.Fragment>
         <Head>
           <meta
@@ -27,7 +25,6 @@ export default function App({ Component, pageProps }: AppProps) {
         </Head>
           <Component {...pageProps} />
       </React.Fragment>
-    </SSRProvider >
   )
 }
 
