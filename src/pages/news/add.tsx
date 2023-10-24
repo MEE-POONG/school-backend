b1a23e04348e02746a1e04a67faa01d4f0a14425
devilzeros <devilzeros@outlook.co.th>
2023-10-24 14:44:54 +0700

    est

--- src/pages/news/add.tsx
+++ src/pages/news/add.tsx
@@ -4,6 +4,7 @@ import AddModal from "@/components/modal/AddModal";
 import useAxios from "axios-hooks";
 import axios from "axios";
 import LayOut from "@/components/RootPage/TheLayOut";
+import { CKEditor } from "ckeditor4-react";
 
 
 const NewsSchoolAdd: React.FC = () => {
@@ -16,124 +17,83 @@ const NewsSchoolAdd: React.FC = () => {
           {/* <AddModal checkAlertShow={alertForm} setCheckAlertShow={setAlertForm} checkBody={checkBody} pathBack={"/newsSchool"} /> */}
           <Card.Header className="d-flex space-between">
             <h4 className="mb-0 py-1">
-              เพิ่มข่าว
+              เพิ่มข่าว / กิจกรรม
             </h4>
           </Card.Header>
           <Card.Body>
             <Row>
-              {/* <Col md={4}>
-                <FloatingLabel controlId="NewsName" label="ชื่อข่าว * จำกัด 50 ตัวอักษร" className="mb-3" style={{ color: 'red' }}>
-                  <Form.Control
-                    isValid={inputForm && newName !== ""}
-                    isInvalid={inputForm && newName === ""}
-                    type="text"
-                    value={newName}
-                    onChange={e => {
-                      const newValue = e.target.value;
-                      if (newValue.length <= 50) {
-                        setnewName(newValue);
-                      }
-                    }}
-                    placeholder="name@example.com"
-                  />
-                </FloatingLabel>
-              </Col> */}
-              <Col md={4}>
+              <Col md={6}>
                 <FloatingLabel controlId="title" label="หัวข้อ" className="mb-3">
                   <Form.Control
-                    // isValid={inputForm && newTitle !== ""}
-                    // isInvalid={inputForm && newTitle === ""}
                     type="text"
-                  // value={newTitle}
-                  // onChange={e => setnewTitle(e.target.value)}
                   />
                 </FloatingLabel>
               </Col>
-              <Col md={4}>
-                <FloatingLabel controlId="subTitle" label="บทความย่อ" className="mb-3">
+              <Col md={6}>
+                <FloatingLabel controlId="floatingSelect" label="เลือกประเภทข่าวสาร">
+                  <Form.Select aria-label="Floating label select example">
+                    <option value="News">ข่าว</option>
+                    <option value="Relations">ประชาสัมพันธ์</option>
+                    <option value="Activity">กิจกรรม</option>
+                    <option value="Interested">สนใจเข้าศึกษา</option>
+                    <option value="Current">นักศึกษาปัจจุบัน</option>
+                    <option value="AlumniServices">บริการศิษย์เก่า</option>
+                    <option value="WorkWithUs">ร่วมงานกับเรา</option>
+                  </Form.Select>
+                </FloatingLabel>
+              </Col>
+              <Col md={6}>
+                <FloatingLabel controlId="startDate" label="วันเริ่มกิจกรรม" className="mb-3">
                   <Form.Control
-                    // isValid={inputForm && newTitle !== ""}
-                    // isInvalid={inputForm && newTitle === ""}
-                    type="text"
-                  // value={newTitle}
-                  // onChange={e => setnewTitle(e.target.value)}
+                    type="date"
                   />
                 </FloatingLabel>
               </Col>
-              <Col md={4}>
-                <FloatingLabel controlId="type" label="ประเภทข่าว" className="mb-3">
+              <Col md={6}>
+                <FloatingLabel controlId="endDate" label="วันเสิ้นสุดกิจกรรม" className="mb-3">
                   <Form.Control
-                    // isValid={inputForm && newTitle !== ""}
-                    // isInvalid={inputForm && newTitle === ""}
-                    type="text"
-                  // value={newTitle}
-                  // onChange={e => setnewTitle(e.target.value)}
+                    type="date"
                   />
                 </FloatingLabel>
               </Col>
-              <Col md={4}>
+              <Col md={6}>
                 <FloatingLabel controlId="img" label="รูปภาพ" className="mb-3">
                   <Form.Control
-                    // isValid={inputForm && newImg !== null}
-                    // isInvalid={inputForm && newImg === null}
                     type="file"
-                    // defaultValue={newImg}
-                    // onChange={handleFileUpload}
-                    // placeholder="NewsImg"
                   />
                 </FloatingLabel>
               </Col>
-              {/* <Col md={4}>
-                <FloatingLabel controlId="NewsSubTitle" label="หัวข้อย่อย" className="mb-3">
+              <Col md={6}>
+                <FloatingLabel controlId="promoteImg" label="รูปภาพสไลด์" className="mb-3">
                   <Form.Control
-                    isValid={inputForm && newSubTitle !== ""}
-                    isInvalid={inputForm && newSubTitle === ""}
-                    type="text"
-                    value={newSubTitle}
-                    onChange={e => setnewSubTitle(e.target.value)}
-                    placeholder="NewsSubTitle"
-                  />
-                </FloatingLabel>
-              </Col> */}
-
-              {/* <Col md={4}>
-                <FloatingLabel controlId="NewsDate" label="วันที่ " className="mb-3">
-                  <Form.Control
-                    isValid={inputForm && newDate !== ""}
-                    isInvalid={inputForm && newDate === ""}
-                    type="date"
-                    value={newDate}
-                    onChange={e => setnewDate(e.target.value)}
-                    placeholder="NewsDate"
-                  />
-                </FloatingLabel>
-              </Col> */}
-              {/* <Col md={4}>
-                <FloatingLabel controlId="NewsImg" label="รูปภาพ" className="mb-3">
-                  <Form.Control
-                    isValid={inputForm && newImg !== null}
-                    isInvalid={inputForm && newImg === null}
                     type="file"
-                    // defaultValue={newImg}
-                    onChange={handleFileUpload}
-                    placeholder="NewsImg" />
+                  />
                 </FloatingLabel>
-              </Col> */}
-            </Row>
-
-            {/* <Col md={8}>
-              <FloatingLabel controlId="NewsSubDetail" label="รายละเอียดข่าว" className="mb-3">
-                <Form.Control
-                  as="textarea"
-                  isValid={inputForm && newSubDetail !== ""}
-                  isInvalid={inputForm && newSubDetail === ""}
-                  value={newSubDetail}
-                  onChange={e => setnewSubDetail(e.target.value)}
-                  placeholder="NewsSubDetail"
-                  style={{ width: "100%", height: "200px" }}
+              </Col>
+              <Col md={6}>
+                <FloatingLabel controlId="subTitle" label="บทความย่อ" className="mb-3">
+                  <Form.Control
+                    as="textarea"
+                    style={{ height: '300px' }}
+                  />
+                </FloatingLabel>
+              </Col>
+              <Col md={6}>
+                <CKEditor
+                  // initData={fromAbout?.subhistory}
+                  // onChange={(e) => {
+                  //   setFromAbout((oldState) => {
+                  //     return { ...oldState, subhistory: e.editor.getData() }
+                  //   })
+                  // }}
+                  config={{
+                    uiColor: "#ddc173 ",
+                    language: "th",
+                    extraPlugins: "easyimage,autogrow,emoji",
+                  }}
                 />
-              </FloatingLabel>
-            </Col> */}
+              </Col>
+            </Row>
           </Card.Body>
           <Card.Footer className="text-end">
             {/* <Button variant="success mx-2" onClick={handleSubmit}>
