8a216069783dc5606b9cb931069496aee9ab6339
Ploy <117616170+yaminP@users.noreply.github.com>
2023-11-29 11:28:44 +0700

    _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import type { AppProps } from 'next/app'
 import { useRouter } from "next/router";
 import '../scss/globals.scss'
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   useEffect(() => {
